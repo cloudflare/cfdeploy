6dfccb246f4c7c672732508b2e20689aeab32443
Brandon Nelson <bnelson@cloudflare.com>
2018-07-10 15:20:23 -0700

    Adds optional healthcheck command and test

--- marathon_test.go
+++ marathon_test.go
@@ -34,6 +34,8 @@ apps:
     healthChecks:
       - protocol: HTTP
         path: /_healthcheck
+        command:
+          value: foo
         gracePeriodSeconds: 3
         intervalSeconds: 10
         portIndex: 0
@@ -107,6 +109,36 @@ func TestMarathonParseYAMLPorts(t *testing.T) {
 			yaml:         "apps: [{portDefinitions: [0, 0]}]",
 			expectError:  true,
 			validateFunc: nil,
+		},
+	}
+	for i, test := range tests {
+		config, err := marathonParseYAML([]byte(test.yaml))
+		switch {
+		case err != nil && test.expectError:
+		case err == nil && test.expectError:
+			t.Errorf("expected error, didn't get it")
+		case err != nil:
+			t.Errorf("unexpected error in test %v: %v", i, err)
+		case !test.validateFunc(&config):
+			t.Errorf("invalid result for test %v: %#v", i, config)
+		}
+	}
+}
+
+
+func TestMarathonParseYAMLHealthCheck(t *testing.T) {
+	tests := []struct {
+		yaml         string
+		expectError  bool
+		validateFunc func(*marathonGroup) bool
+	}{
+		{
+			yaml: "apps: [{healthChecks: [{protocol: COMMAND, command: {value: foo}, gracePeriodSeconds: 2, intervalSeconds: 2, portIndex: 1, timeoutSeconds: 2, maxConsecutiveFailures: 2 }]}]",
+			validateFunc: func(g *marathonGroup) bool {
+				hc := g.Apps[0].HealthChecks[0]
+				return hc.Protocol == "COMMAND" && hc.Command.Value == "foo" && hc.GracePeriodSeconds == 2 &&
+					hc.IntervalSeconds == 2 && hc.PortIndex == 1 && hc.TimeoutSeconds == 2 && hc.MaxConsecutiveFailures == 2
+			},
 		},
 	}
 	for i, test := range tests {
