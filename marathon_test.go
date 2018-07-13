f4f04482beca16586cdf59945afc8bb3835ed1b3
Brandon Nelson <bnelson@cloudflare.com>
2018-07-13 15:28:03 -0700

    Removes validateFunc from test per PR feedback

--- marathon_test.go
+++ marathon_test.go
@@ -127,9 +127,8 @@ func TestMarathonParseYAMLPorts(t *testing.T) {
 
 func TestMarathonYAMLtoJSON(t *testing.T) {
 	tests := []struct {
-		yaml         []byte
-		json         []byte
-		validateFunc func(group *marathonGroup) bool
+		yaml []byte
+		json []byte
 	}{
 		{
 			yaml: []byte(``),
@@ -146,11 +145,6 @@ func TestMarathonYAMLtoJSON(t *testing.T) {
 		{
 			yaml: []byte(`apps: [{healthChecks: [{protocol: COMMAND, command: {value: foo}, gracePeriodSeconds: 2, intervalSeconds: 2, portIndex: 1, timeoutSeconds: 2, maxConsecutiveFailures: 2 }]}]`),
 			json: []byte(`{"id":"","apps":[{"id":"","instances":0,"cpus":0,"mem":0,"constraints":null,"requirePorts":false,"container":{"type":"","volumes":null},"healthChecks":[{"protocol":"COMMAND","command":{"value":"foo"},"gracePeriodSeconds":2,"intervalSeconds":2,"portIndex":1,"timeoutSeconds":2,"maxConsecutiveFailures":2}]}]}`),
-			validateFunc: func(g *marathonGroup) bool {
-				hc := g.Apps[0].HealthChecks[0]
-				return hc.Protocol == "COMMAND" && hc.Command.Value == "foo" && hc.GracePeriodSeconds == 2 &&
-					hc.IntervalSeconds == 2 && hc.PortIndex == 1 && hc.TimeoutSeconds == 2 && hc.MaxConsecutiveFailures == 2
-			},
 		},
 	}
 	for i, test := range tests {
@@ -168,9 +162,6 @@ func TestMarathonYAMLtoJSON(t *testing.T) {
 				test.json,
 				marathonJSON,
 			)
-		}
-		if test.validateFunc != nil && !test.validateFunc(&config) {
-			t.Errorf("invalid result for test %v: %#v", i, config)
 		}
 	}
 }
